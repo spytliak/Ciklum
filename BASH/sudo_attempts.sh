621ed5d8a81f9f401400a69167fa45592eb23f7e
Serhii Pytliak <serhii.pytliak@netcracker.com>
2021-07-29 12:16:59 +0300

    added some info

--- BASH/sudo_attempts.sh
+++ BASH/sudo_attempts.sh
@@ -2,17 +2,19 @@
 #title            :sudo_attempts.sh
 #description      :This script counts how many times each user assumed root privileges
 #author           :Serhii Pytliak
-#date             :20210728
+#date             :20210729
 #version          :0.1
 #==============================================================================
 
+# TODO
+# Check all users who can login and root privileges
+# Grep sudo from log 
 
-# Check all users who can login
+# Check all users who can login 
 cat /etc/passwd |grep bash |awk -F':' '{ print $1}'
 
-# Check root privileges
+# Check root privileges 
  getent group sudo | cut -d: -f4 |sed 's/,/\n/g'
 
-
-
+# Example log with sudo output
 Jul 26 06:45:42 ps sudo:       ps : TTY=pts/0 ; PWD=/home/ps ; USER=root ; COMMAND=/usr/bin/date
